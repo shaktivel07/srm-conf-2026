7482dece61c929f477fdc091513537d24661f575
Shaktivel kumaresan <shaktivelkumaresan07@gmail.com>
2026-07-06 14:41:27 +0530

    paper submission

--- src/app/submissions/page.jsx
+++ src/app/submissions/page.jsx
@@ -4,7 +4,7 @@ export default function SubmissionsPage() {
     return (
         <div className="flex flex-col items-center justify-center px-4 min-h-[50vh]">
             <div className="container mx-auto px-4 py-20 md:max-w-[70%]">
-                <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">Submissions</h1>
+                <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">Paper Submission</h1>
                 <div className="bg-white shadow-lg rounded-lg p-8">
                     <p className="text-lg text-gray-700 leading-relaxed mb-8">
                         The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
